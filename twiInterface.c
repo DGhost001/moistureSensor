5e62056535c8a9cd41fbfe514b8080a1f5afca2e
Falk 'DGhost' Ahlendorf <falk.ahlendorf@googlemail.com>
2022-07-11 13:30:42 +0200

    TWI interface implementation

--- twiInterface.c
+++ twiInterface.c
@@ -8,8 +8,12 @@
 #define SDA DDB0
 #define SCL DDB2
 
+#ifndef ISR
+    #define ISR(X) void X(void)
+#endif
+
 enum{
-  maxBufferSize = 8
+  maxBufferSize = 16
 };
 
 enum TwiStatus {
@@ -19,6 +23,7 @@ enum TwiStatus {
     twiSendData,
     twiSendAck,
     twiWaitAck,
+    twiRequestAck,
 };
 
 struct Buffer{
@@ -48,6 +53,21 @@ static void usiOutput(bool const out)
     }
 }
 
+static void usiWaitForStart( void )
+{
+    USICR = (1 << USISIE) | (0 << USIOIE) | /* Enable Start Condition Interrupt. Disable Overflow Interrupt.*/
+            (1 << USIWM1) | (0 << USIWM0) | /* Set USI in Two-wire mode. No USI Counter overflow hold.      */
+            (1 << USICS1) | (0 << USICS0) | (0 << USICLK) | /* Shift Register Clock Source = External, positive edge */
+            (0 << USITC);
+}
+
+static void usiWaitForOverflow( void )
+{
+    USICR = (1 << USISIE) | (1 << USIOIE) | /* Enable Start Condition Interrupt. Disable Overflow Interrupt.*/
+            (1 << USIWM1) | (1 << USIWM0) | /* Set USI in Two-wire mode. No USI Counter overflow hold.      */
+            (1 << USICS1) | (0 << USICS0) | (0 << USICLK) | /* Shift Register Clock Source = External, positive edge */
+            (0 << USITC);
+}
 
 static void usiPrepareAck( void )
 {
@@ -72,10 +92,7 @@ static void usiPrepareReadAck( void )
 static void usiSetToStartCondition( void )
 {
     usiOutput(false);
-    USICR = (1 << USISIE) | (0 << USIOIE) | /* Enable Start Condition Interrupt. Disable Overflow Interrupt.*/
-            (1 << USIWM1) | (0 << USIWM0) | /* Set USI in Two-wire mode. No USI Counter overflow hold.      */
-            (1 << USICS1) | (0 << USICS0) | (0 << USICLK) | /* Shift Register Clock Source = External, positive edge */
-            (0 << USITC);
+    usiWaitForStart();
     usiSetUsIsr(8);
 }
 
@@ -131,16 +148,118 @@ void twiInitialize(uint8_t const address)
 {
     internalState_ = twiWaitForStart;
     ownAddress_ = address;
+
+    PORTB |= (1<<SCL) | (1<<SDA); //Set SCL and SDA to high
+    DDRB  |= (1<<SCL);            //Set SCL to Output
+
+    /* Reset all Interrupts */
+    USISR = 0xF0;
+
+    usiSetToStartCondition();
+
 }
 
 ISR(USI_START_vect)
 {
-    USISR = 0; //Reset the USI Status, with the detection of the Start Condition
-    USICR = USICR | (1<<USIOIE); //Enable the overflow condition
     internalState_ = twiWaitForAddress; //We received the START, now wait for the address
+
+    unsigned sclPin;
+
+    /* Wait here for SCL to also go low, or SDA to go high again */
+    for(sclPin = PINB;
+        (sclPin & (1<<SCL | 1 << SDA)) == (1<<SCL);
+        sclPin = PINB
+        ) {}
+
+    if(sclPin & (1<<SCL)) {
+        usiWaitForStart();
+    } else {
+        usiWaitForOverflow();
+    }
+
+    /* Reset all Interrupts  and flags*/
+    USISR = 0xF0;
 }
 
 ISR(USI_OVF_vect)
 {
+    uint8_t const dataByte = USIDR;
 
+    switch(internalState_)
+    {
+    case twiWaitForAddress:
+    {
+        //Check if we are addressed at all ...
+        if((dataByte == 0) || ((dataByte >> 1) == ownAddress_))
+        {
+            //The address is our address ... do we have to send or receive?
+            internalState_ = dataByte & 1 ? twiSendData : twiSendAck;
+            usiPrepareAck(); //Acknowledge the reception of the Address
+        } else {
+            usiSetToStartCondition(); //We are not addressed ... so sleep again ...
+            internalState_ = twiWaitForStart;
+        }
+
+        break;
+    }
+
+    case twiSendAck:
+    {
+        internalState_ = twiWaitForData;
+        usiSetToReadData();
+        break;
+    }
+    case twiWaitForData:
+    {
+        internalState_ = twiSendAck;
+        unsigned const nextWrite = next(rxBuffer_.write);
+
+        if(nextWrite != rxBuffer_.read)
+        {
+            rxBuffer_.buffer[rxBuffer_.write] = dataByte;
+            rxBuffer_.write = nextWrite;
+            usiPrepareAck();
+        } else {
+            usiPrepareNack();
+        }
+
+        break;
+    }
+
+    case twiWaitAck:
+    {
+        if(dataByte) //We received an NACK
+        {
+            usiSetToStartCondition(); //Go into start condition again ...
+            break;
+        }
+    } //fall through
+    case twiSendData:
+    {
+        //Do we have data ...
+        if(txBuffer_.read != txBuffer_.write)
+        {
+            USIDR = txBuffer_.buffer[txBuffer_.read];
+            txBuffer_.read = next(txBuffer_.read);
+            internalState_ = twiRequestAck;
+            usiSetToSendData();
+        } else {
+            internalState_ = twiWaitForStart;
+            usiSetToStartCondition();
+        }
+        break;
+    }
+
+    case twiRequestAck:
+    {
+        internalState_ = twiWaitAck;
+        usiPrepareReadAck();
+        break;
+    }
+
+    default:
+        internalState_ = twiWaitForStart;
+        usiSetToStartCondition();
+        break;
+    }
 }
